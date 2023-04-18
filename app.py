ef2131609e6a738c2fcf888237cdc341e2409499
mafemad <mafemad2005@gmail.com>
2023-04-18 15:33:50 -0300

    ordem das paginas

--- app.py
+++ app.py
@@ -10,21 +10,29 @@ def index():
 def introducao():
     return render_template('introducao.html')
 
-@app.route('/sprint')
-def sprint():
-    return render_template('sprint.html')
-
-@app.route('/po')
-def po():
-    return render_template('po.html')
-
 @app.route('/importancia')
 def imp():
     return render_template('importancia.html')
 
-@app.route('/productbacklog')
-def pbacklog():
-    return render_template('productbacklog.html')
+@app.route('/sprint')
+def sprint():
+    return render_template('sprint.html')
+
+@app.route('/SprintBacklog')
+def sprintbacklog():
+    return render_template('SprintBacklog.html')
+
+@app.route('/mvp')
+def mvp():
+    return render_template('mvp.html')
+
+@app.route('/productincrement')
+def PI():
+    return render_template('productincrement.html')
+
+@app.route('/po')
+def po():
+    return render_template('po.html')
 
 @app.route('/sm')
 def sm():
@@ -34,17 +42,17 @@ def sm():
 def equipe():
     return render_template('equipe.html')
 
+@app.route('/productbacklog')
+def pbacklog():
+    return render_template('productbacklog.html')
+
 @app.route('/dor-dod')
 def dor_dod():
     return render_template('dor-dod.html')
 
-@app.route('/SprintBacklog')
-def sprintbacklog():
-    return render_template('SprintBacklog.html')
-
-@app.route('/productincrement')
-def PI():
-    return render_template('productincrement.html')
+@app.route('/burndown')
+def burndown():
+    return render_template('burndown.html')
 
 @app.route('/planningpoker')
 def PP():
@@ -53,14 +61,6 @@ def PP():
 @app.route('/pacer')
 def pacer():
     return render_template('pacer.html')
-
-@app.route('/burndown')
-def burndown():
-    return render_template('burndown.html')
-
-@app.route('/mvp')
-def mvp():
-    return render_template('mvp.html')
 
 @app.route('/skills')
 def skills():
